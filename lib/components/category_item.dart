49ac510f9491a7b5d0210dbdd0f9d6d524921bda
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-11-12 16:45:20 -0300

    first commit

---
+++ lib/components/category_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final Category category;
+
+  const CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(category.title),
+    );
+  }
+}
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+
+import '../models/category.dart';
+import '../components/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
