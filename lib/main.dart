95c713f545188ddbbd58002641d2e04407a90164
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-11-17 20:24:39 -0300

    Parâmetros para as Configurações e adições no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+
+import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'utils/app_routes.dart';
+import 'screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,22 +17,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink,).copyWith(
-          secondary: Colors.amber,
-        ),
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          subtitle1: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.pink,
+          ).copyWith(
+            secondary: Colors.amber,
           ),
-        )
-      ),
-      home: CategoriesScreen(),
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                subtitle1: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  color: Colors.white,
+                ),
+              )),
       routes: {
-        '/categories-meals': (ctx) => CategoriesMealsScreen(context),
+        AppRoutes.HOME: (context) => TabsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(context),
+        AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (context) => SettingsScreen(),
       },
+      // função de teste que posso usar para criar rotas dinâmicas baseadas no NOME da ROTA
+      // [
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == '/outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(
+      //       builder: (_) {
+      //         return CategoriesScreen();
+      //       },
+      //     );
+      //   // ]
+      //   }
+      // },
+      // onUnknownRoute: (settings) {},
+      debugShowCheckedModeBanner: false,
     );
   }
 }
